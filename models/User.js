4819be5f8d2738749963dded959a2e25a6c8b903
Patrick Nymark <pn@ctdenmark.dk>
2019-06-14 13:31:44 +0200

    created staff model and finished user model

--- models/User.js
+++ models/User.js
@@ -3,9 +3,32 @@ const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
-  name: String,
-  email: String,
-  password: String
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+      type: String,
+      required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 255
+  },
+  roles: {
+    staff: { type: Schema.Types.ObjectId, ref: 'staff' }
+  }
+}, {
+  timestamps: true
 });
 
 UserSchema.pre("save", function(next) {
@@ -22,13 +45,5 @@ UserSchema.pre("save", function(next) {
     });
   });
 })
-
-UserSchema.methods.comparePassword = function(plainPassword, cb) {
-  bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
-    if (err) return cb(err);
-
-    return cb(null, isMatch);
-  });
-};
 
 module.exports = User = mongoose.model('users', UserSchema);
