5f3c8c39a0fb3827c6018821be316dcca945cd94
Patrick Nymark <pn@ctdenmark.dk>
2019-06-14 13:32:22 +0200

    created role authorization helper

---
+++ helpers/role-auth.js
@@ -0,0 +1,30 @@
+const jwt = require('express-jwt');
+const isEmpty = require('../helpers/is-empty.js');
+
+function authorize(roles = []) {
+    // roles param can be a single role string (e.g. Role.User or 'User') 
+    // or an array of roles (e.g. [Role.Admin, Role.User] or ['Admin', 'User'])
+    if (typeof roles === 'string') {
+        roles = [roles];
+    }
+
+    console.log(roles);
+
+    return [
+        // authenticate JWT token and attach user to request object (req.user)
+        jwt({ secret: process.env.secretOrKey }),
+
+        // authorize based on user role
+        (req, res, next) => {
+            // staff authorization
+            if(isEmpty(req.user.role.staff) && roles.includes('staff'))  {
+              return res.status(401).json({ message: 'Unauthorized' });
+            }
+
+            // authentication and authorization successful
+            next();
+        }
+    ];
+}
+
+module.exports = authorize;
